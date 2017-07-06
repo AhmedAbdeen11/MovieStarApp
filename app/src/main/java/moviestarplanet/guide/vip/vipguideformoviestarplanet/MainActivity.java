082e3e4e9d19f9351662583b42de91ad329fd2bd
A7MED-Freelancer <aabdeen@fcih1.com>
2017-07-06 18:36:26 +0200

    Adding Toast Commit

--- app/src/main/java/moviestarplanet/guide/vip/vipguideformoviestarplanet/MainActivity.java
+++ app/src/main/java/moviestarplanet/guide/vip/vipguideformoviestarplanet/MainActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "aaa", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "aaa", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "aaa", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "aaa", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "aaa", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "aaa", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "aaa", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "aaa", Toast.LENGTH_SHORT).show();
     }
 
     public void tips(View view) {
